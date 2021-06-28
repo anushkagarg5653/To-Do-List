3779b11654036a435d8f23be103b6687332eeb0a
anushkagarg5653 <anushkagarg0811@gmail.com>
2021-06-28 12:54:21 +0530

    migrated from redux to redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import './App.css';
-import React from "react";
-import AddTodo from './components/AddTodo/AddTodo';
-import List from './components/List/List';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AddTodoForm from './components/AddTodoForm';
+import TodoList from './components/TodoList';
+import TotalCompleteItems from './components/TotalCompleteItems';
 
-
-function App() {
-  return (
-    <div className="App">
-    <h1>TODO - LIST</h1>
-      <AddTodo />
-      <List />
-    </div>
-  );
-}
+const App = () => {
+	return (
+		<div className='container bg-white p-4 mt-5'>
+			<h1>My Todo List</h1>
+			<AddTodoForm />
+			<TodoList />
+			<TotalCompleteItems />
+		</div>
+	);
+};
 
 export default App;
